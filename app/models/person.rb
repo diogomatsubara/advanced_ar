485d3068490560c476fa2b18dbb73bd628ef4953
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-05 14:27:19 -0300

    add Person.max_salary which uses approx_salaries rich many-to-many relationship

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,8 @@ class Person < ActiveRecord::Base
   has_and_belongs_to_many :hobbies
   has_many :jobs
   has_many :approx_salaries, through: :jobs, source: :salary_range
+
+  def max_salary
+    approx_salaries.maximum(:max_salary)
+  end
 end
