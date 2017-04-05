437588fac43a5dc99d4e5cda5dcc64e61a90fc19
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-05 15:05:51 -0300

    add custom validator to SalaryRange (salary_min can't be greater than salary_max)

--- app/models/salary_range.rb
+++ app/models/salary_range.rb
@@ -1,3 +1,13 @@
 class SalaryRange < ActiveRecord::Base
   belongs_to :job
+
+  # :numericality validator provides this function but this is just an example
+  # of a custom validator
+  validate :min_is_less_than_max
+
+  def min_is_less_than_max
+    if min_salary > max_salary
+      errors.add(:min_salary, "cannot be greater than maximum salary!")
+    end
+  end
 end
