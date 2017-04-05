114aa9a36a209a6e2f7dd3dab135388905ebe585
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-04 22:31:35 -0300

    add one_to_one relationship from job to salary_range and add relationship from person to salary_range through jobs

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 class Person < ActiveRecord::Base
   has_one :personal_info, dependent: :destroy
-  has_many :jobs
   has_many :my_jobs, class_name: "Job"
   has_and_belongs_to_many :hobbies
+  has_many :jobs
+  has_many :approx_salaries, through: :jobs, source: :salary_range
 end
